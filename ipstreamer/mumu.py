2d6a58a78d597516f8477f770c7467d09aee50d8
Tijmen de Mes <tijmen@vck.utwente.nl>
2013-10-23 16:08:27 +0200

    test

--- ipstreamer/mumu.py
+++ ipstreamer/mumu.py
@@ -3,8 +3,6 @@ from application.notification import IObserver, NotificationCenter
 from application.python import Null
 from application.python.types import Singleton
 
-from eventlib import api, coros, proc
-
 from threading import Lock
 from twisted.internet import reactor
 from zope.interface import implements
@@ -18,10 +16,5 @@ class MumuApplication(object):
 
 
     def start(self):
-        notification_center = NotificationCenter()
-        notification_center.add_observer(self, name='SystemIPAddressDidChange')
-        notification_center.add_observer(self, name='SystemDidWakeUpFromSleep')
-        self._select_proc = proc.spawn(self._process_files)
-        proc.spawn(self._handle_commands)
         self._activate()
 
